0572a0c87a76715a7086555fa4c8de20e6871d87
BorysPedorenko <boriszhenya@gmail.com>
2025-05-04 17:21:20 +0200

    Add Cucumber Reporter

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,18 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "steps"
+        glue = "steps",
+        plugin = {
+
+                "pretty",
+
+                "html:target/cucumber-reports/cucumber.html",
+
+                "json:target/cucumber-reports/cucumber.json",
+
+                "junit:target/cucumber-reports/cucumber.xml"
+
+        }
 )
 public class TestRunner {
 }
